e202461caa7b62d65316f46de8df0d966efc9e2f
Christian Rios Mancilla <criosmancilla@gmail.com>
2026-02-18 21:34:08 -0500

    chore(admin): remove model registrations from Django admin modules

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -1,23 +1,3 @@
 from django.contrib import admin
 
-from apps.chat.models import Conversation, Message
-
-
-class MessageInline(admin.TabularInline):
-    model = Message
-    extra = 0
-
-
-@admin.register(Conversation)
-class ConversationAdmin(admin.ModelAdmin):
-    list_display = ("id", "item", "created_by", "updated_at")
-    search_fields = ("item__title", "created_by__email")
-    filter_horizontal = ("participants",)
-    inlines = [MessageInline]
-
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ("id", "conversation", "sender", "is_read", "created_at")
-    list_filter = ("is_read",)
-    search_fields = ("content", "sender__email")
+# Model admin registrations intentionally omitted.
--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
 
-from apps.core.models import UserActivity
-
-
-@admin.register(UserActivity)
-class UserActivityAdmin(admin.ModelAdmin):
-    list_display = ("activity_type", "user", "item", "page_path", "created_at")
-    list_filter = ("activity_type", "created_at")
-    search_fields = ("user__email", "search_query", "page_path")
+# Model admin registrations intentionally omitted.
